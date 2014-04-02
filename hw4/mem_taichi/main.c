915753598bc6456e4b2be44db5c054dee362ed77
jacyxli <jacy.xli@gmail.com>
2014-04-02 01:37:36 -0400

    Tested

--- hw4/mem_taichi/main.c
+++ hw4/mem_taichi/main.c
@@ -9,38 +9,8 @@ int m_error;
 
 int main(int argc, char** argv) {
     
-
-    Mem_Init(8192);
-    
-    
     printf("============ Starting test =============\n");
-
-        
-    //check_for_eight_byte();
-    //simple_8_byte_alloc();
-    //aligned_alloc();
-    //odd_sized_alloc();
-    //bad_args_to_mem_init();
-    //worst_fit_alloc();
-    //coalesce_of_space();
-    simple_alloc_free();
-
-
-    /*
-    Mem_Init(4096);
-    
-    Mem_Dump();
-    
-    printf("Before\n");
-    Mem_Alloc(8);
-    printf("In mem alloc\n");
-    Mem_Alloc(500);   
-    Mem_Dump();
-    */
-    
- 
-    //printf("Starting test\n");
-    //odd_sized_alloc_free();   
+    init_size_one_page();
     return 0;
 
 }
