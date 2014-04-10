057f67f57f0d70025b59504c789ca144bf23189e
Panda <stmishu@gmail.com>
2014-04-10 16:09:19 -0400

    [Panda] Test fread

--- hw5/fragdozer/defrag.c
+++ hw5/fragdozer/defrag.c
@@ -1,17 +1,40 @@
 #include <stdio.h>
 
+#define SUPER_ITEMS 6
+#define BLOCK_BASE 1024
+#define INODE_ITEMS 25
+
 int main(int argc, char** argv) {
     
     FILE* fp;
     fp = fopen("datafile-frag", "r");
     
-    fseek(fp, 512, SEEK_SET);
+    fseek(fp, 512, SEEK_SET); //Skip the first 512 byte
     
-    int test;
+    int super_block[SUPER_ITEMS];    
+    fread((void*)super_block, sizeof(int), SUPER_ITEMS, fp);
     
-    fread((void*)&test, sizeof(int), 1, fp);
+    int i;
+    for(i = 0; i < SUPER_ITEMS; i++) {
+        printf("%d\n", super_block[i]);
+    }
     
-    printf("Read is %d", test);
+    int block_size = super_block[0];
+    int inode_pt = BLOCK_BASE + super_block[1];
+    int data_pos = BLOCK_BASE + super_block[3];
+    
+    
+  //  do {
+        int inode[INODE_ITEMS];
+        fread((void*)inode, sizeof(int), INODE_ITEMS, fp);
+        
+        
+
+        
+        
+    //} while();
+        
+    
     
     
     
