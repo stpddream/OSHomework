64f3e32f296494462f9e0c919035d1d4d0d0f365
Panda <stmishu@gmail.com>
2014-05-05 11:31:48 -0400

    [Panda] start with file table

--- hw6/tiandi_filesys/file.c
+++ hw6/tiandi_filesys/file.c
@@ -1,5 +1,5 @@
 #include "file.h"
-#include "filesys_hd.h"
+
 
 Dev* cur_dev;
 
@@ -8,14 +8,19 @@ Dev* cur_dev;
 int f_open(const char* path, const char* mode) {
     char* dirs;
     
-    /*
+    
     int inode_idx = fs_alloc_inode(cur_dev);
     iNode* inode = fs_get_inode(cur_dev, inode_idx);
     
     inode->nlink = 1;
-   // inode->file_type = ;
+    inode->file_type = FT_FILE;
+    inode->mtime = time(0);
+    inode->ctime = time(0);
+    inode->atime = time(0);
+    inode->name = path;
+    inode->size = 0;
     fs_update_inode(cur_dev, inode);
-    */
+    
     /*
     if(path[0] == '/') {
         //Set current directory
