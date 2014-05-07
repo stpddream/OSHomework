066cd65afe941d302b04453abcd220ad3f9ae0a2
jacyxli <jacy.xli@gmail.com>
2014-05-06 20:13:00 -0400

    under construction

--- hw6/tiandi_filesys/test_main.c
+++ hw6/tiandi_filesys/test_main.c
@@ -65,170 +65,53 @@ int main(int argc, char** argv) {
     f_mkdir("/good/quite/haha");
     f_mkdir("/good/quite/so");
     
-    f_open("/good/super.c", "w");
-    f_open("/good/hahah.o", "w");
-            
-    
-
-
-    
-   
-    printf("Root name is : %s\n", root.name);
-    printf("Root size is: %d\n", root.size);
-    
-    printf("now listing...\n");
-    list_dir(&root);
-    int good_idx = dir_lookup(&root, "good");
-    
-    printf("Processing good\n");
-    iNode good;
-    fs_get_inode(&good, good_idx, cur_dev);
-    list_dir(&good);
-    
-    int quite_idx = dir_lookup(&good, "quite");
-    iNode quite;
-    fs_get_inode(&quite, quite_idx, cur_dev);
-    list_dir(&quite);
-    
-    printf("===================================\n");
-    
-    
-    printf("readf here\n");
-    /* Read file doesn't exist */
     /*
-    int readf = f_open("/superfile.h", "r");    
-    printf("Readf %d\n", readf);
-    char* perfect = "hohoho";
-    printf("Status is %d\n", f_write(perfect, sizeof(perfect), 1, readf));
-    */
-
-    /* Read to write only file */    
+    int fd = f_open("/good/quite/test.c", "r");
+    f_close(fd);*/
     
-    int writef = f_open("/supergreat.h", "w");
-    printf("what about writef %d\n", writef);
-    char* stuff = "great job\n";
-    printf("size of stuff %d\n", sizeof(stuff));
-    f_write(stuff, sizeof(stuff), 1, writef);
-    char readout[20];        
-    printf("Status: %d \n", f_read(readout, sizeof(stuff), 1, writef));   
+    int lflag, fflag;
     
-
-        
-    int inode_idx = ft_get_idx(writef);
-
-    iNode* inode = it_get_node(inode_idx);
-    printf("size of node %d", inode->size);
-    printf("Inode another here %d\n", inode->file_type);        
-    f_close(writef);
+    lflag = FALSE;
+    fflag = TRUE;
     
-    fl_read(cur_dev, inode, 0, sizeof(stuff), readout);
-    printf("read out something?? %s\n", readout);
-    
-    
-    print_filetable();
-   
-    /* Write to read only file */
-    
-    printf("whwhwhwhwhw\n");
-    int readf = f_open("/supergreat.h", "r");
-    printf("read f is %d\n", readf);
-    f_read(readout, sizeof(stuff), 1, readf);
-    printf("Read out!! %s\n", readout);
-    printf("seg!!\n");
-    
-    
-    //Test iNode table
-    /*    printf("Inode table test\n");
-    iNode* inode = (iNode*)malloc(sizeof(iNode));
-    printf("ioioi\n");
-    inode->file_type = 23;
-    it_put(inode, 23);
-    printf("before??\n");
-    
-    iNode* hahanode = it_get_node(23);
-    printf("hahahahho %d\n", hahanode->file_type);
-    
-    */
-    
-    /*
-    iNode another;    
-    fs_get_inode(&another, 4, cur_dev);       
-    printf("file name is %s\n", another.name);
-    
+    char* curdir = "/good/quite";
+    DirStream* dir_stream = f_opendir(curdir);
     DirFileEntry entry;
-    fl_read(cur_dev, &root, 0, DIR_ENTRY_SZ, &entry);
+    iNode test_node;
+    char display[FILE_NAME_MAX+1];
     
-    
-    printf("Entry name is %s\n", entry.file_name);        
-    f_open("/file/quick/ha", "w");
-    
-    
-    int sub_idx = dir_lookup(&root, "good");
-    printf("index is %d\n", sub_idx);
-    */
-    
-    
-    
-    
-    
-    
-    /*
-    int inode_idx = fs_alloc_inode(cur_dev);    
-    iNode inode;
-    int fd = f_open("super.h", "r");
-    
-    int num = 46;
-    
-    char* good = "this is a very long long long long long long long long long long string";
-    f_write(good, sizeof(good), 1, fd);
-    
-    printf("File Descr %d\n", fd);
-    
-    char a[300];
-    f_read(&a, sizeof(good), 1, fd);
-    
-    printf("read out %s\n", good);
-    
-    
-    char perfect[10] = "hahahahaha";
-    int another_idx = fs_alloc_inode(cur_dev);
-    iNode another_inode;
-    fs_get_inode(&another_inode, another_idx, cur_dev);
-    f_write(perfect, sizeof(perfect), 1, fd);
-    
-    char b[200];
-    f_read(&b, sizeof(perfect), 1, fd);
-    printf("another: %s\n", b);
-    */
-    
-    
-    
-    
-    
-
-    
-    
-    /*
-    fseek(cur_dev->phys_data, 9728, SEEK_SET);     
-    
-    for(i = 0; i < 20; i++) {
-        fread(&inode, 128, 1, cur_dev->phys_data);
-        printf("inode blabla %d\n", inode.file_type);
-    }*/    
-    
-//  printf("FD is %d\n", fd);
-
-/*
-    DirStream* res = f_opendir("/");
-    DirFileEntry dir;
-    printf("open dir inode = %d\n", res->inode_idx);
-    printf("open dir pos = %d\n", res->pos);
-    
-    while(f_readdir(res, &dir) != -1){
-        printf("filename: %s\n", dir.file_name);
+    if(!lflag && !fflag){
+        while(f_readdir(dir_stream, &entry) != -1){
+            if(strcmp(entry.file_name, ".") == 0) continue;
+            if(strcmp(entry.file_name, "..") == 0) continue;
+            strcpy(display, entry.file_name);
+            printf("%s\t", display);
+        }
+        printf("\n");
+    }else if(fflag && !lflag){
+        while(f_readdir(dir_stream, &entry) != -1){
+            if(strcmp(entry.file_name, ".") == 0) continue;
+            if(strcmp(entry.file_name, "..") == 0) continue;
+            strcpy(display, entry.file_name);
+            fs_get_inode(&test_node, entry.inode_idx, cur_dev);
+            if(test_node.file_type == FT_FILE){
+                strcat(display, "*");
+            } else if (test_node.file_type == FT_DIR){
+                strcat(display, "/");
+            } else if (test_node.file_type == FT_MOUNT){
+                strcat(display, "@"); 
+            }
+            printf("%s\t", display);
+        }
+        printf("\n");        
+    }else if(!fflag && lflag){
+        
+    }else{
+        
     }
-    
-    f_closedir(res);*/
+           
+    f_closedir(dir_stream);
+
     return (EXIT_SUCCESS);
 }
 
