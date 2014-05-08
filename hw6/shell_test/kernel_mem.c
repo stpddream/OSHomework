b1acb255f9f65a5ba1728b09c9accb5429632e19
Panda <stmishu@gmail.com>
2014-05-07 22:15:49 -0400

    [Panda] Remove everything.

--- hw6/shell_test/kernel_mem.c
+++
@@ -1,9 +0,0 @@
-#include "kernel_mem.h"
-
-
-User* user_create(char* uname, int priv) {
-    User* new_user = (User*)malloc(sizeof(User));
-    strcpy(new_user->name, uname);
-    new_user->privilege = priv;
-    return new_user;
-}
